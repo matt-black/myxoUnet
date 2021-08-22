fa6d132bbbaa6c5d4e24c324cb7717565fa0fdb1
matt <matt.black7@gmail.com>
2021-08-22 14:51:41 -0400

    jupyter script for della

---
+++ jupyter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --time=00:05:00
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=jup-nb
+
+# get tunneling info
+XDG_RUNTIME_DIR=""
+node=$(hostname -s)
+user=$(whoami)
+cluster="della-gpu"
+port=8889
+
+# print tunneling instructions jupyter-log
+echo -e "
+Command to create ssh tunnel:
+ssh -N -f -L ${port}:${node}:${port} ${user}@${cluster}.princeton.edu
+
+Use a Browser on your local machine to go to:
+localhost:${port}  (prefix w/ https:// if using password)
+"
+
+# load modules or conda environments here
+module purge
+module load anaconda3/2020.11
+conda activate pyt
+
+# Run Jupyter
+jupyter-notebook --no-browser --port=${port} --ip=${node}
