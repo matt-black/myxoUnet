13ac1b2f98dedf826091e69511421a6acc6d64c2
Katherine Copenhagen <kc32@princeton.edu>
2021-09-20 21:14:04 -0400

    Adding 5th label to training scripts.

--- unet/train.sh
+++ unet/train.sh
@@ -7,15 +7,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:45:00
 #SBATCH --mail-type=end
-#SBATCH --mail-user=mb46@princeton.edu
+#SBATCH --mail-user=kc32@princeton.edu
 
 module purge
 module load anaconda3/2020.11
-conda activate pyt
+conda activate cells
 
-python train.py --data="../supp/test" --data-global-stats \
-       --loss="wce" --num-classes=4 --crop-size=256 \
-       --learning-rate=.0001 --batch-size=2 --epochs=100 \
+python train.py --data="/home/kc32/trainingdata/20210917border" --data-global-stats \
+       --loss="dsc" --num-classes=5 --crop-size=260 \
+       --learning-rate=.0001 --batch-size=2 --epochs=500 \
        --reduce-lr-plateau="0.1,10,0.0001", \
-       --unet-depth=3 --unet-wf=6 --unet-batchnorm --unet-upmode="upconv" \
-       --print-freq=1 --save
+       --unet-depth=5 --unet-wf=6 --unet-batchnorm --unet-upmode="upconv" \
+       --print-freq=1 --save 
